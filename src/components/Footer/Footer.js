2e027553b527e2f7a3cef263a795f50b247d2b7c
Ayushi Bhargava <88292134+Ayushi512@users.noreply.github.com>
2022-07-12 01:15:11 +0530

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://www.linkedin.com/in/ayushibhargava/"
+        style={{ cursor: "pointer" }}
+      >
+        Ayushi Bhargava
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
